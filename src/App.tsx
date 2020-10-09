e4dd0c817c6b663f199134c56b0c46c21cd722d8
jaqarrick <pigmaask@gmail.com>
2020-10-08 17:02:48 -0700

    updated router

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Blog from "./components/blog/Blog"
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom"
 
 const App = () => (
-  <Router>
+  <Router basename={process.env.PUBLIC_URL + "/"}>
     <Switch>
       <Route path='/posts' exact component={Blog} />
       <Route path='/' exact component={Home} />
